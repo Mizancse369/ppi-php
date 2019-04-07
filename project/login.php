c3ffca3dbf0637de8b6656711db0a4d46e3bf5ad
Muhammad Sumon Molla Selim <smseleem@gmail.com>
2019-04-07 13:37:44 +0600

    user registration added

---
+++ project/login.php
@@ -0,0 +1,11 @@
+<?php $title = 'Login';
+session_start();
+require_once 'connection.php';
+require_once 'partials/_header.php';
+?>
+
+<main role="main">
+
+</main>
+
+<?php require_once 'partials/_footer.php'; ?>
